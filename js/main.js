0e45e370be5d428dca1e30423fec500d4829ef71
Egor Yurchenko <admegor@mail.ru>
2022-05-17 09:03:29 +0500

    modal part II

--- js/main.js
+++ js/main.js
@@ -168,6 +168,8 @@ window.addEventListener('resize', () => {
     promoHeight();
 });
 
+// Slider
+
 $(document).ready(function(){
     let mainSliderNname = document.querySelector('.main__slider-name');
 
@@ -203,3 +205,64 @@ $(document).ready(function(){
     
 })
 
+// modals
+
+const popupLinks = document.querySelectorAll('.popup-link');
+const body = document.querySelector('body');
+const lockPadding = document.querySelectorAll('.lock-padding');
+
+let unlock = true;
+
+const timeout = 800;
+
+for (let i = 0; i < popupLinks.length; i++) {
+    const popupLink = popupLinks[i];
+    popupLink.addEventListener('click', function(e) {
+        const popupName = popupLink.getAttribute('href').replace('#', '');
+        const currentPopup = document.getElementById(popupName);
+        popupOpen(currentPopup);
+        e.preventDefault();
+        console.log(popupLinks.dataset.theme)
+    })
+}
+
+const popupCloseIcon = document.querySelectorAll('.close-popup');
+
+if (popupCloseIcon.length > 0) {
+    for (let i = 0; i < popupCloseIcon.length; i++) {
+        const el = popupCloseIcon[i];
+        el.addEventListener('click', function(e) {
+            popupClose(el.closest('.popup-pixel'));
+            e.preventDefault();
+        })
+    }
+}
+
+function popupOpen(currentPopup) {
+    if (currentPopup && unlock) {
+        const popupActive = document.querySelector('.popup-pixel.open');
+        if (popupActive) {
+            popupClose(popupActive, false)
+        }
+
+        currentPopup.classList.add('open');
+        currentPopup.addEventListener('click', function(e) {
+            if (!e.target.closest('.popup-pixel__content')) {
+                popupClose(e.target.closest('.popup-pixel'));
+            }
+        })
+    }
+}
+
+function popupClose(popupActive, doUnlock = true) {
+    if (unlock) {
+        popupActive.classList.remove('open');
+    }
+}
+
+document.addEventListener('keydown', function(e) {
+    if (e.which === 27) {
+        const popupActive = document.querySelector('.popup-pixel.open');
+        popupClose(popupActive);
+    }
+})
